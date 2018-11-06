9b01f90d41df0d68b4fac8bbcde61ef3a1a9844b
vivek panchal <vivekpanchal64@gmail.com>
2018-11-06 15:09:31 +0530

    GCE step done

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,10 +3,12 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.vivek.panchal.javajoker.Joker;
 
-import javax.inject.Named;
 
-/** An endpoint class we are exposing */
+/**
+ * An endpoint class we are exposing
+ */
 @Api(
         name = "myApi",
         version = "v1",
@@ -18,13 +20,23 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+//    @ApiMethod(name = "sayHi")
+//    public MyBean sayHi(@Named("name") String name) {
+//        MyBean response = new MyBean();
+//        response.setData("Hi, " + name);
+//
+//        return response;
+//    }
+    @ApiMethod(name = "tellJoke")
+    public MyBean tellJoke() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
+        Joker joker = new Joker();
+        response.setData(joker.tellJoke());
         return response;
     }
+
 
 }
---
+++ jokefactory/src/main/java/com/vivek/panchal/jokefactory/DisplayJokeActivity.java
@@ -0,0 +1,13 @@
+package com.vivek.panchal.jokefactory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayJokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_joke);
+    }
+}
